58284c9295784a83dd4497fb291c96b6c8717ed7
Jorge Londoño <jorge.londono@gmail.com>
2025-06-04 10:26:46 -0500

    docs: update TODO comment to specify exercise for unit test creation

--- src/c05_SORT/MergeSort.py
+++ src/c05_SORT/MergeSort.py
@@ -5,5 +5,5 @@ def MergeSort(a: list) -> list:
 
 
 
-# TODO write a unit test showing the correct operation of the sorting algorithm
+# TODO Exercise: write a unit test showing the correct operation of the sorting algorithm
 
