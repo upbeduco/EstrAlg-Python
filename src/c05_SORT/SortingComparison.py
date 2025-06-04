78b1e181c2eaad27d495aff51289a61f57504853
Jorge Londoño <jorge.londono@gmail.com>
2025-06-04 10:32:59 -0500

    Compare sorting

---
+++ src/c05_SORT/SortingComparison.py
@@ -0,0 +1,7 @@
+
+# TODO function to create a list of n random integers
+
+# TODO function compare sorting algorithms: Create list, copy list per algorithm, run algorithm, report the name of the algorithm and its running time.
+
+# TODO main executing the comparison
+
